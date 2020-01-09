4c04b47bcfb34f4a37d902b579822e0f99c557d6
awesomelemonade <awesomelemonade@gmail.com>
2020-01-08 21:50:36 -0500

    Added tryShootDrone() to HQBot

--- citricsky/battlecode2020/HQBot.java
+++ citricsky/battlecode2020/HQBot.java
@@ -21,6 +21,9 @@ public class HQBot implements RunnableBot {
 		}
 		MapLocation currentLocation = controller.getLocation();
 		turn: {
+			if (tryShootDrone()) {
+				break turn;
+			}
 			for (int i = 0; i < Util.FLOOD_FILL_DX.length; i++) {
 				MapLocation location = currentLocation.translate(Util.FLOOD_FILL_DX[i], Util.FLOOD_FILL_DY[i]);
 				if (!controller.canSenseLocation(location)) {
@@ -49,5 +52,17 @@ public class HQBot implements RunnableBot {
 			controller.buildRobot(RobotType.MINER, direction);
 			this.spawnCount++;
 		}
+	}
+	public boolean tryShootDrone() throws GameActionException {
+		RobotInfo[] enemies = controller.senseNearbyRobots(controller.getLocation(),
+				GameConstants.NET_GUN_SHOOT_RADIUS_SQUARED, controller.getTeam().opponent());
+		for (RobotInfo enemy : enemies) {
+			int enemyId = enemy.getID();
+			if (controller.canShootUnit(enemyId)) {
+				controller.shootUnit(enemyId);
+				return true;
+			}
+		}
+		return false;
 	}
 }
