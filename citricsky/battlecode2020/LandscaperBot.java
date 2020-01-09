3240972855a6e682b87724329fdccf1c874b04f2
awesomelemonade <awesomelemonade@gmail.com>
2020-01-08 23:04:39 -0500

    Added pathfinding to enemyHQ in LandscaperBot

--- citricsky/battlecode2020/LandscaperBot.java
+++ citricsky/battlecode2020/LandscaperBot.java
@@ -1,21 +1,22 @@
 package citricsky.battlecode2020;
 
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 import citricsky.battlecode2020.util.InfoMap;
+import citricsky.battlecode2020.util.Pathfinding;
 import citricsky.battlecode2020.util.Util;
 
 public class LandscaperBot implements RunnableBot {
 	private RobotController controller;
 	private MapLocation enemyHQ;
 	private InfoMap infoMap;
+	private Pathfinding pathfinding;
 	public LandscaperBot(RobotController controller) {
 		this.controller = controller;
 	}
 	@Override
 	public void init() {
 		infoMap = new InfoMap(controller.getMapWidth(), controller.getMapHeight());
+		pathfinding = new Pathfinding(controller);
 	}
 	@Override
 	public void turn() throws GameActionException {
@@ -31,8 +32,21 @@ public class LandscaperBot implements RunnableBot {
 				break;
 			}
 		}
-		Util.randomWalk();
 		// Find enemy HQ (by exploring)
+		if (enemyHQ == null) {
+			RobotInfo[] enemyUnits = controller.senseNearbyRobots(-1, controller.getTeam().opponent());
+			for (RobotInfo enemy : enemyUnits) {
+				if (enemy.getType() == RobotType.HQ) {
+					this.enemyHQ = enemy.getLocation();
+					break;
+				}
+			}
+		}
 		// Dig trench from water to hq
+		if (enemyHQ == null) {
+			Util.randomWalk();
+		} else {
+			pathfinding.execute(enemyHQ);
+		}
 	}
 }
