c785fe813de463952b2ba06164b0275d07307341
awesomelemonade <awesomelemonade@gmail.com>
2020-01-20 15:54:08 -0500

    Restrict Miner Pathfinding to lattice further

--- citricsky/battlecode2020/util/Pathfinding.java
+++ citricsky/battlecode2020/util/Pathfinding.java
@@ -141,7 +141,7 @@ public class Pathfinding {
 				// Check if toLocation is within hq vision range
 				if (!MinerBot.hqLocation.isWithinDistanceSquared(location, RobotType.HQ.sensorRadiusSquared)) {
 					// Check if toElevation is lower than lower bound
-					if (toElevation < lower) {
+					if (toElevation < lower || toElevation > upper) {
 						// If it's not within vision range AND elevation is lower
 						return false;
 					}
